8ca6e7a4c5ead4886741281bf2369fce1a7f35d7
linhhm <hmailinh9@gmail.com>
2025-03-29 23:55:34 +0700

    Sửa cấu hình deploy và build

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' 
-    ? '/website-online/' 
+  base: process.env.NODE_ENV === 'production' 
+    ? '/website-online/'  // Thay 'website-online' bằng tên repository của bạn
     : '/'
-}
+});
