8341b29b184fd4f9a7cb9afb992c4af458b8a860
linhhm <hmailinh9@gmail.com>
2025-04-19 23:35:06 +0700

    ok

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/website-online/', // Thêm base path cho GitHub Pages (thay 'website-online' bằng tên repo của bạn)
 })
